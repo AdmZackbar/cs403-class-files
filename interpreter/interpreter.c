c79febe61211aa90c656e1d8abe3677da45e73f9
Zach Wassynger <zach.wassynger@gmail.com>
2019-02-22 13:24:29 -0600

    Hopefully fixed arg parsing in closures

--- interpreter/interpreter.c
+++ interpreter/interpreter.c
@@ -291,7 +291,13 @@ static LEXEME *evalClosure(LEXEME *closure, LEXEME *args)
 {
     LEXEME *staticEnv = car(closure);
     LEXEME *params = cdr(cdr(cdr(closure)));
-    // TODO - add support for opt args
+    LEXEME *paramIter = params;
+    while (paramIter)
+    {
+        // TODO add support for opt args
+        setCar(car(car(paramIter)));    // Update the value in the list to be ID, not VAR_DEF
+        paramIter = cdr(paramIter);
+    }
     LEXEME *localEnv = newScopeEnv(staticEnv, params, args);
     LEXEME *body = car(cdr(closure));
     LEXEME *result = evalStatements(body, localEnv);
