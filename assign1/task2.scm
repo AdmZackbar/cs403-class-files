f2bbbb5f321e64fa0b58ada8c7bc3651ef96c620
Zacharighteous <zawassynger@crimson.ua.edu>
2019-03-01 11:03:46 -0600

    Finalized task2(assign1)

--- assign1/task2.scm
+++ assign1/task2.scm
@@ -1,3 +1,22 @@
-(define (mandelbrot x)
+(define (mandelbrot threshold)
     (lambda (x y)
+        (define (iter r s numIter)
+            (cond
+                ((> (+ (* r r) (* s s)) 4) numIter)
+                ((>= numIter threshold) 0)
+                (else
+                    (iter (+ (- (* r r) (* s s)) x) (+ (* 2 r s) y) (+ numIter 1))
+                    )
+                )
+            )
+            (iter 0.0 0.0 0)
         )
+    )
+
+(define (main)
+    (setPort (open (getElement ScamArgs 1) 'read))
+    (define threshold (readInt))
+    (define x (readReal))
+    (define y (readReal))
+    (println "((mandelbrot " threshold ") " x " " y ") is " ((mandelbrot threshold) x y))
+    )
