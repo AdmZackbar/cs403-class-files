ce7c14896362e15d2958f8a226272fb7bdde0957
Zach Wassynger <zach.wassynger@gmail.com>
2019-03-04 13:58:48 -0600

    Adjusted task9(assign1)

--- assign1/task9i.scm
+++ assign1/task9i.scm
@@ -1,33 +1,30 @@
 (define (ramanujani x)
-    (define (iter i)
+    (define (iter term value)
         (cond
-            ((> i x) 0.0)
-            (else
-                (* (+ 1.0 i) (sqrt (+ 6.0 i (iter (+ i 1)))))
-                )
+            ((< term 0) value)
+            (else (iter (- term 1) (* (+ 1 term) (sqrt (+ 6 term value)))))
             )
         )
-    (iter 0)
+    (iter x 0)
     )
 
-(define (latexR x)
-    (define (iter i)
+(define (latexI x)
+    (define (iter term value)
         (cond
-            ((> i x) 0)
-            ((= i x)
-                (string+ "" (+ 1 i) "\\cdot\\sqrt{" (+ 6 i) "}")
-                )
-            (else
-                (string+ "" (+ 1 i) "\\cdot\\sqrt{" (+ 6 i) "+" (iter (+ i 1)) "}")
-                )
-            )
+            ((< term 0) value)
+            ((= term x) (iter (- term 1) (string+ "" (+ 1 term) "\\cdot\\sqrt{" (+ 6 term) "}")))
+            (else (iter (- term 1) (string+ "" (+ 1 term) "\\cdot\\sqrt{" (+ 6 term) "+" value "}")))
         )
-    (string+ "$" (iter 0) "$")
+    (iter x nil)
     )
+
+(string+ "" (+ 1 i) "\\cdot\\sqrt{" (+ 6 i) "}")
+(string+ "" (+ 1 i) "\\cdot\\sqrt{" (+ 6 i) "+" (recur (+ i 1)) "}")
+
 
 (define (main)
     (setPort (open (getElement ScamArgs 1) 'read))
     (define arg1 (readInt))
     (println "(ramanujani " arg1 ") is " (fmt "%.25f" (ramanujani arg1)))
-    (println (latexR arg1))
+    (println (latexI arg1))
     )
--- assign1/task9r.scm
+++ assign1/task9r.scm
@@ -1,14 +1,33 @@
 (define (ramanujanr x)
-    (cond
-        ((= x 0) (* 1 (sqrt 6)))
-        (else
-            
+    (define (recur i)
+        (cond
+            ((> i x) 0.0)
+            (else
+                (* (+ 1.0 i) (sqrt (+ 6.0 i (recur (+ i 1)))))
+                )
             )
         )
+    (recur 0)
+    )
+
+(define (latexR x)
+    (define (recur i)
+        (cond
+            ((> i x) 0)
+            ((= i x)
+                (string+ "" (+ 1 i) "\\cdot\\sqrt{" (+ 6 i) "}")
+                )
+            (else
+                (string+ "" (+ 1 i) "\\cdot\\sqrt{" (+ 6 i) "+" (recur (+ i 1)) "}")
+                )
+            )
+        )
+    (string+ "$" (recur 0) "$")
     )
 
 (define (main)
     (setPort (open (getElement ScamArgs 1) 'read))
     (define arg1 (readInt))
-    ;(println "(ramanujanr " arg1 ") is " (fmt "%.25f" (ramanujanr arg1)))
+    (println "(ramanujanr " arg1 ") is " (fmt "%.25f" (ramanujanr arg1)))
+    (println (latexR arg1))
     )
