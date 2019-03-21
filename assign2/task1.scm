bf7c59ac085cbad636c2fedec011ca2f95b68e64
Ubuntu <zawassynger@crimson.ua.edu>
2019-03-21 18:17:04 +0000

    Finalized assign2-task1

--- assign2/task1.scm
+++ assign2/task1.scm
@@ -19,6 +19,7 @@
     )
 
 (define (main)
+    (setPort (open (getElement ScamArgs 1) 'read))
     (define env this)
     (define (iter expr)
          (if (not (eof?)) (begin (eval expr env) (iter (readExpr))))
