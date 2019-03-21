8f8b8479754492b0469db37aea2404e6d7e7fdf7
Zach Wassynger <zach.wassynger@gmail.com>
2019-03-21 12:59:11 -0500

    Added for-loop to assign2-task1

--- assign2/task1.scm
+++ assign2/task1.scm
@@ -8,6 +8,17 @@
     (iter start)
     )
 
+(define (for-loop list procedure)
+    (define (iter arg_list return)
+        (if (null? arg_list)
+            return
+            (iter (cdr arg_list) (procedure (car arg_list)))
+            )
+        )
+    (iter (car list nil))
+    )
+
 (define (main)
     (inspect (range 0 10 1))
+    (for-loop (range 0 10 5) (lambda (x) (inspect x)))
     )
