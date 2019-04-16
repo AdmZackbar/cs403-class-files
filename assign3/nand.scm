022ed05642824ebf0590ced424e96f0039f486ea
Zacharighteous <zawassynger@crimson.ua.edu>
2019-04-16 00:46:29 -0500

    Added various changes from sub0

--- assign3/nand.scm
+++ assign3/nand.scm
@@ -1,7 +1,12 @@
 (define (nand-gate a1 a2 output)
     (define (nand-action-procedure)
-        (let ((new-value (logical-nand (get-signal a1) (get-signal a2))))
-            (after-delay nand-gate-delay (lambda () (set-signal! output new-value)))
+        (let ((new-value (logical-not (logical-and (get-signal a1) (get-signal a2)))))
+            (after-delay nand-gate-delay
+                (lambda ()
+                    ;(print "\nNAND activated!")
+                    (set-signal! output new-value)
+                    )
+                )
             )
         )
     (add-action! a1 nand-action-procedure)
@@ -10,7 +15,3 @@
     )
 
 (define nand-gate-delay 6)
-
-(define (logical-nand a b)
-    (if (= a b 1) 0 1)
-    )
